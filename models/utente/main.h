88b654c3393c9022ef5cdc14ff7494e0078801a7
Mattia Di Marco <mattiadmboomd22@gmail.com>
2024-01-30 15:24:49 +0100

    New Update
    Modificata la classe utente

---
+++ models/utente/main.h
@@ -0,0 +1,26 @@
+//
+// Created by simolb on 1/29/24.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+#include <con2redis.h>
+#include <pgsql.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#define READ_STREAM "stream2"
+#define WRITE_STREAM "stream1"
+
+/*#define NAME  "Marco"
+#define SURNAME string "Rossi"
+#define EMAIL string "marcorossi69@gmail.com"
+#define PASSWORD string "redigotham"
+#define PURCHTYPE ["Paypal", "Mastercard", "Visa", "BitCoin", "ETH"]*/
+
+
+
+#endif //MAIN_H
---
+++ models/utente/registration.cpp
@@ -0,0 +1,6 @@
+#include <main.h>
+#include <utente.h>
+
+void registration(Con2DB db1, Utente utente){
+    
+};
